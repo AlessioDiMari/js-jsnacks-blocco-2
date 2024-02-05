e48c23acd832e10005f7daa92c8c2c0cb8728479
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-05 17:58:53 +0100

    snack1, tramite button faccio la funzione

---
+++ Snack 1/script.js
@@ -0,0 +1,22 @@
+/*
+Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
+Continua a chiedere i numeri all’utente e a inserirli nell’array
+fino a quando la somma degli elementi è minore di 50
+*/
+
+// Salvo il button
+const insertButton = document.getElementById("inserisci");
+
+// Creo una funzione che svverrà al "click" del button
+insertButton.addEventListener("click",
+
+
+function(){
+    
+
+
+
+}
+
+
+)
