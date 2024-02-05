c363e8a29187ac146f7bc9f5721a6c0393004cec
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-05 18:14:33 +0100

    fine snack 1

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -7,6 +7,9 @@ fino a quando la somma degli elementi è minore di 50
 // Salvo il button
 const insertButton = document.getElementById("inserisci");
 
+// Salvo il risultato della somma
+let result = document.getElementById("risultato")
+
 // Creo una funzione che svverrà al "click" del button
 insertButton.addEventListener("click",
 
@@ -24,10 +27,28 @@ function(){
     let sumNumbers = 0;
 
     while( sumNumbers < 50 ){
-        
+
+        // Dichiaro il numero da far inserire con un prompt
+
+        let number = Number(prompt("Inserisci un numero"));
+
+        // Verifico se la variabile inserita sia un numero
+        if (!isNaN(number)){
+            // Inserisco la variabile nell'array
+            numbers.push(number);
+
+            // Aggiungo alla somma il valore della variabile
+            sumNumbers += number;
+
+        } else {
+            // Se la variabile inserita non è un numero lo comunico all'utente
+            alert("Il valore inserito non è un numero")
+        }
+
     }
 
-
+    // Stampo la somma risultante
+    result.innerHTML = `La somma é ${sumNumbers}`;
 }
 
 
