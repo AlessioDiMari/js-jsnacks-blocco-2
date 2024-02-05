be5021ead9a89c534cf22f6a633d4a26ed0f91b9
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-05 19:05:05 +0100

    interseco i do while e verifico il tentativo dell'utente

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -22,6 +22,8 @@ insertButton.addEventListener("click",
         // Faccio generare un numero casuale al computer
         const computerGuess = Math.floor(Math.random() * 100) + 1;
 
+        console.log(computerGuess);
+
         // Utilizzo due cicli "do while"
         // uno per verificare che l'utente scriva un numero
         // e un altro per verificare se ha indovinato il numero del computer
@@ -30,18 +32,39 @@ insertButton.addEventListener("click",
 
         let userGuess;
 
+        // verifico il tentativo dell'utente finché non indovina
+
         do{
-            // Verifico se l'utente scriva un numero e 
-            // in caso lo avverto con un alert
-            userGuess = Number(prompt("Prova ad indovinare"));
 
-            if (isNaN(userGuess)){
-                alert("Inserisci un numero!")
+            do{
+                // Verifico se l'utente scriva un numero e 
+                // in caso lo avverto con un alert
+                userGuess = Number(prompt("Prova ad indovinare"));
+    
+                if (isNaN(userGuess)){
+                    alert("Inserisci un numero!")
+                }
+                        
+            } while (isNaN(userGuess));
+
+            if ( userGuess < computerGuess){
+
+                // condizione per cui l'uente inserisce un numero
+                // inferiore a quello del computer
+                alert("Il numero è troppo piccolo. Riprova");
+
+            } else if ( userGuess > computerGuess){
+
+                // condizione per cui l'uente inserisce un numero
+                // maggiore a quello del computer
+                alert("Il numero è troppo grande. Riprova");
+
+            } else if ( userGuess == computerGuess){
+                // condizione in cui l'utente indovina
+                result.innerHTML = `Hai indovinato!!! La soluzione è proprio ${userGuess}`;
             }
-                    
-        } while (isNaN(userGuess));
 
-
+        } while ( userGuess != computerGuess);
 
     }
 
